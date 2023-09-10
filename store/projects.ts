167655c360efd9c8dca38c52d5e5479631cef37a
tchi-sama <tchisama41@gmail.com>
2023-09-10 09:59:51 -0700

    adding the cactusia project

--- store/projects.ts
+++ store/projects.ts
@@ -11,6 +11,63 @@ interface Project {
 const projects: Project[] = [
   {
     image:
+      "https://firebasestorage.googleapis.com/v0/b/tchisama-1cc2f.appspot.com/o/projects%20images%2FFrame%207.png?alt=media&token=1f8bddf2-9ff4-460c-8307-1236b87634a0",
+    name: "Cactusia",
+    urlName: "cactusia",
+    subtitle: "online cactus store",
+    readme: ` 
+### Cactusia: Your Handmade Cactus Store
+
+![Frame 8 (2)](https://github.com/cactussia/cactusia/assets/115560200/352b789b-5e90-43cb-ba68-b8c7b886f35f)
+
+
+## Overview
+
+**Cactusia** is a unique and delightful online cactus store where cactus enthusiasts can shop for handpicked and handcrafted cactus plants. This project brings a creative twist to e-commerce by allowing users to not only select their preferred cactus but also customize its pot color to see the final product before making a purchase. **Cactusia** is developed using ReactJS, Vite, Firebase, Material-UI (MUI), and Tailwind CSS.
+
+## Project Links
+
+- Website: [Cactusia.ma](https://www.cactusia.ma)
+- Instagram: [Cactusia.ma](https://www.instagram.com/cactusia.ma/)
+- Facebook: [Cactusia.ma](https://www.facebook.com/cactusia.ma/)
+
+## Key Features
+
+### 1. Unique Cactus Selection
+
+**Cactusia** offers a wide variety of cactus plants, each handpicked and carefully curated. Users can explore the collection and select their favorite cacti for purchase.
+
+### 2.  Pot Color
+
+A standout feature of **Cactusia** is the ability for users to customize the color of the cactus pot. This interactive feature allows customers to visualize how their chosen cactus will look in their preferred pot color.
+
+### 3. User-Friendly Design
+
+The user interface is designed to be intuitive and visually appealing, providing a seamless shopping experience for cactus enthusiasts.
+
+## Technology Stack
+
+**Cactusia** is built using the following technologies:
+
+- **ReactJS**: A popular JavaScript library for building user interfaces, providing a responsive and dynamic front-end experience.
+- **Vite**: A fast, lightweight build tool for modern web development.
+- **Firebase**: A cloud-based platform that provides essential backend services, including authentication and real-time database functionality.
+- **Material-UI (MUI)**: A popular React UI framework that offers pre-built components and styles for a consistent and attractive design.
+- **Tailwind CSS**: A utility-first CSS framework that enhances the overall user interface and design.
+
+## Collaboration with "whybe"
+
+**Cactusia** owes much of its success to the valuable collaboration with "whybe." Together, we combined our skills and expertise to create an outstanding online cactus store. "whybe" made significant contributions to the project, and you can find more of their work on their GitHub profile: [whybe's GitHub](https://github.com/vvhybe). We are grateful for their partnership and dedication to making "Cactusia" a reality.
+
+## Conclusion
+
+**Cactusia** is not just an online store; it's an experience for cactus lovers. With its unique cactus selection, interactive pot color customization, and user-friendly design, it provides a delightful shopping experience. Explore **Cactusia** today and bring home a beautiful, handcrafted cactus plant.
+
+[Link to Cactusia Website](https://www.cactusia.ma)
+`,
+  },
+  {
+    image:
       "https://firebasestorage.googleapis.com/v0/b/tchisama-1cc2f.appspot.com/o/projects%20images%2FFrame%201%20(5).png?alt=media&token=7e508471-ed5e-4e84-a252-98bd1dd54baa",
     name: "NodeCode",
     urlName: "nodecode",
