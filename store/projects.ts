842264514ac2bcd9ebecd62dc63c3438a17809a8
tchi-sama <tchisama41@gmail.com>
2023-09-26 10:46:47 -0700

    adding the ai template project

--- store/projects.ts
+++ store/projects.ts
@@ -346,6 +346,70 @@ AI Translator is your passport to a world without language barriers. Embrace glo
     
     `,
   },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/tchisama-1cc2f.appspot.com/o/projects%20images%2FFrame%2013.png?alt=media&token=ac1d02e9-3ef5-465b-82c6-389e7be56fdb",
+    name: "Ai templates",
+    urlName: "aitemplates",
+    subtitle: "Ai powered template generator",
+    readme: `
+
+---
+
+# AI HTML Template Generator: Unleash Your Web Design Creativity
+![Frame 14](https://github.com/tchisama/ai-template/assets/115560200/7127cb1d-9e93-4041-a1d3-cd00dc47c688)
+
+Welcome to the AI HTML Template Generator, where innovation meets design, and web development becomes a breeze!
+
+## **Getting Started: Seamless Login**
+
+Embark on your creative journey with ease by logging in using your Google, email, or GitHub account. Securely access your projects and ideas, setting the stage for a world of endless possibilities.
+
+## **Your Creative Hub: The Dashboard**
+
+Upon login, you'll find yourself in your personal haven - the dashboard. This is where you orchestrate your web design symphonies, each sketch representing a unique masterpiece.
+
+- **Dashboard Overview**: Gain immediate access to your creations and projects. Manage them effortlessly, all in one place.
+
+- **Energize Your Creativity**: Keep an eye on your Usage Points, the currency of our platform. Accumulate more points by actively using the platform or inviting friends to join your creative revolution.
+
+- **Start Fresh**: Ready to breathe life into a new vision? Simply hit the "Create New" button, and you'll be transported to our AI-powered playground, where your imagination knows no boundaries.
+
+## **The Playground: Where Magic Happens**
+
+- **Template Generation**: Our cutting-edge AI system is at your fingertips. Describe your vision in the prompt input, and witness it transform into a fully-fledged HTML template or component. Say goodbye to countless hours of manual coding!
+
+- **Craft with Precision**: Flexibility is paramount. If the initial output doesn't quite match your vision, you wield the power to fine-tune every detail. Adjust colors, styles, or introduce new elements effortlessly right within the playground.
+
+- **Framework Freedom**: Whether you're a devotee of React, Vue, Angular, or traditional HTML, our AI adapts to your needs. Choose your preferred framework, and watch as the generated code seamlessly aligns with your vision.
+
+## **Tech Stack: The Brains Behind the Beauty**
+
+Here's the powerful technology stack that brings our platform to life:
+
+- **Frontend**: Next.js, Tailwind CSS, shadCn UI
+- **Backend**: Node.js, Express
+- **Database**: MongoDB
+- **Authentication**: ClerkAuth
+- **AI**: OpenAI APIs
+
+## **Join the Revolution: Transform Design Forever**
+
+- **Empower Creativity**: The AI HTML Template Generator empowers designers and developers alike to explore their creative depths without being confined by coding skills.
+
+- **Efficiency Redefined**: Save valuable time and resources by swiftly prototyping and generating components effortlessly.
+
+- **Seamless Collaboration**: Collaboration becomes second nature. Share your sketches, iterate, and collaborate seamlessly with team members.
+
+Unleash your boundless creativity with the AI HTML Template Generator and watch your design ideas come to life effortlessly. Begin your journey today and create web templates like never before!
+
+**GitHub Repositories:**
+- [AI Template Generator](https://github.com/tchisama/ai-template)
+- [AI Template Generator Server](https://github.com/tchisama/ai-template-server)
+
+---
+    `,
+  },
 ];
 
 const useStore = create(() => ({
